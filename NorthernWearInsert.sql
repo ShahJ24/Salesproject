b07cde2ca7b12e745eb7af6fbdeec080806aa474
Shah Jeenal <75457847+ShahJ24@users.noreply.github.com>
2021-04-28 03:35:26 -0400

    added composer.json file

--- NorthernWearInsert.sql
+++ NorthernWearInsert.sql
@@ -2,13 +2,13 @@
 Use NorthernWear;
 
 
-insert into product VALUES(NULL,'Women Winter Jacket', 'Women', 10,150, LOAD_FILE('C:\Users\jeena\OneDrive\Pictures\Camera Roll\jeenal.jpg'));
-insert into product VALUES(NULL,'Women Winter Shoes', 'Women', 25, 200, LOAD_FILE('C:\Users\jeena\OneDrive\Pictures\Camera Roll\jeenal.jpg'));
-insert into product VALUES(NULL,'Women Winter Caps', 'Women', 3, 75, LOAD_FILE('C:\Users\jeena\OneDrive\Pictures\Camera Roll\jeenal.jpg'));
-insert into product VALUES(NULL,'Men Winter Jacket', 'Men', 12, 299, LOAD_FILE('C:\Users\jeena\OneDrive\Pictures\Camera Roll\jeenal.jpg'));
-insert into product VALUES(NULL,'Men Winter Boots', 'Men', 8, 199, LOAD_FILE('C:\Users\jeena\OneDrive\Pictures\Camera Roll\jeenal.jpg'));
-insert into product VALUES(NULL,'Men Winter Muflers', 'Men', 15, 65, LOAD_FILE('C:\Users\jeena\OneDrive\Pictures\Camera Roll\jeenal.jpg'));
-insert into product VALUES(NULL,'Kids Winter Jacket', 'Kids', 9, 175, LOAD_FILE('C:\Users\jeena\OneDrive\Pictures\Camera Roll\jeenal.jpg'));
-insert into product VALUES(NULL,'Kids Winter Gloves', 'Kids', 4, 89, LOAD_FILE('C:\Users\jeena\OneDrive\Pictures\Camera Roll\jeenal.jpg'));
+insert into product VALUES(NULL,'Women Winter Jacket', 'Women', 100,150, LOAD_FILE('C:\xampp\xamppFolder\htdocs\php_project1\images\wwj.jpg'));
+insert into product VALUES(NULL,'Women Winter Shoes', 'Women', 204, 200, LOAD_FILE('C:\xampp\xamppFolder\htdocs\php_project1\images\wws.jpg'));
+insert into product VALUES(NULL,'Women Winter Caps', 'Women', 158, 75, LOAD_FILE('C:\xampp\xamppFolder\htdocs\php_project1\images\wwc.jpg'));
+insert into product VALUES(NULL,'Men Winter Jacket', 'Men', 120, 299, LOAD_FILE('C:\xampp\xamppFolder\htdocs\php_project1\images\mwj.jpg'));
+insert into product VALUES(NULL,'Men Winter Boots', 'Men', 80, 199, LOAD_FILE('C:\xampp\xamppFolder\htdocs\php_project1\images\mwb.jpg'));
+insert into product VALUES(NULL,'Men Winter Muflers', 'Men', 150, 65, LOAD_FILE('C:\xampp\xamppFolder\htdocs\php_project1\images\mwm.jpg'));
+insert into product VALUES(NULL,'Kids Winter Jacket', 'Kids', 90, 175, LOAD_FILE('C:\xampp\xamppFolder\htdocs\php_project1\images\kwj.jpeg'));
+insert into product VALUES(NULL,'Kids Winter Gloves', 'Kids', 40, 89, LOAD_FILE('C:\xampp\xamppFolder\htdocs\php_project1\images\kwg.jpeg'));
 
 select * from product;
